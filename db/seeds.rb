9f122fde7278e1485cb8fd82354a59eb753296e2
JakubowskiA <arieljakubowski@gmail.com>
2019-07-03 14:30:40 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+p1 = Passenger.create(name: "vera")
+p2 = Passenger.create(name: "vera1")
+p3 = Passenger.create(name: "vera2")
+
+t1 = Taxi.create(name: "t1")
+t2 = Taxi.create(name: "t2")
+t3 = Taxi.create(name: "t3")
+
+r1 = Ride.create(passenger_id: p1.id, taxi_id: t1.id)
+r2 = Ride.create(passenger_id: p2.id, taxi_id: t2.id)
+r3 = Ride.create(passenger_id: p1.id, taxi_id: t2.id)
